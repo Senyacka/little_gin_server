aebe7d94bc17ee132d6baebe1b84e3492b9c642b
Senyacka <agent0sh@mail.ru>
2023-12-22 22:39:46 +0500

    First update

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,35 @@ import (
 	"io"
 	"net/http"
 	"os"
+
 	"github.com/gin-gonic/gin"
-	""
 )
+// TODO Добавить Postgres и Redis для хранения данных
+// TODO Добавить регистрацию и авторизацию
+// TODO Добавить логирование действий пользователей
+// TODO Добавить редактирование информации о сайте
+// TODO Добавить отправку формы и загрузку файлов
+// TODO Добавить редактирование информации о пользователе
+// TODO Добавить отправку и получение сообщений пользователю
+
+// Структура для хранения данных о пользователе
+// type User struct {
+// 	ID       	int    `json:"id"`
+// 	Username 	string `json:"username"`
+// 	Password 	string `json:"password"`
+//  Admin	 	bool   `json:"admin"`
+//  Description string `json:"description"`
+// }
+
+
 
 func main() {
 	router := gin.Default()
+
+	// Роутер для отображения главной страницы
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome!")
+	})
 
 	// Роутер для проверки работоспособности API
 	router.GET("/ping", func(c *gin.Context) {
@@ -18,29 +41,30 @@ func main() {
 		})
 	})
 
-	// Роутер для отображения главной страницы
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome!")
+	// Роутер для отображения информации о сайте
+	router.GET("/about", func(c *gin.Context) {
+		c.String(http.StatusOK, "This is a simple Go web app.")
 	})
 
-	// Роутер для отображения случайной страницы Wikipedia
-	router.GET("/wiki", func(c *gin.Context) {
-		resp, err := http.Get(RANDOM_WIKI_HTTPS)
+	// Роутер для загрузки сайта sefus.ru
+	router.GET("/gobook", func(c *gin.Context) {
+		resp, err := http.Get("https://sefus.ru/little-go-book/")
+
 		if err != nil {
-			c.String(http.StatusInternalServerError, "Error fetching Wikipedia page")
+			c.String(http.StatusInternalServerError, "Error downloading page")
 			return
 		}
-		defer resp.Body.Close()
 
+		defer resp.Body.Close()
 		c.Header("Content-Type", "text/html")
 		io.Copy(c.Writer, resp.Body)
 
 	})
 
-	// Роутер для загрузки видео из папки videos
+	// Роутер для стрима видео из папки videos
 	router.GET("/stream/:filename", func(c *gin.Context) {
 		filename := c.Param("filename")
-		file, err := os.Open("videos/" + filename)
+		file, err := os.Open("../videos/" + filename + ".mp4") // Используйте расширение .mp4 для видео в формате MP4
 		if err != nil {
 			c.String(http.StatusNotFound, "Video not found")
 			return
@@ -52,5 +76,21 @@ func main() {
 		io.CopyBuffer(c.Writer, file, buffer)
 	})
 
-	router.Run(":80")
+	// Роутер для вывода изображения из папки images
+	router.GET("/image/:filename", func(c *gin.Context) {
+		filename := c.Param("filename")
+		file, err := os.Open("../images/" + filename + ".jpg") // Используйте расширение .jpg для изображений в формате JPEG
+		if err != nil {
+			c.String(http.StatusNotFound, "Image not found")
+			return
+		}
+		defer file.Close()
+
+		c.Header("Content-Type", "image/jpg")
+		buffer := make([]byte, 64*1024) // 64 KB buffer
+		io.CopyBuffer(c.Writer, file, buffer)
+	})
+
+	// Запуск сервера на порту 8080
+	router.Run(":8080")
 }
--- internal/cfg/config.go
+++
@@ -1,3 +0,0 @@
-package cfg
-
-RANDOM_WIKI_HTTPS = "https://ru.wikipedia.org/wiki/Special:Random"
